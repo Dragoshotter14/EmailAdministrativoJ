c5c483000deff4a6d89ce8a3a3e8fd0a6466b223
Emerson S V <40496678+Dragoshotter14@users.noreply.github.com>
2023-03-01 15:57:08 +0000

    Update EmailAdministrativo.java

--- src/src/EmailAdministrativo.java
+++ src/src/EmailAdministrativo.java
@@ -20,71 +20,78 @@ public class EmailAdministrativo {
             escolha = sc.nextInt();
             switch (escolha) {
                 case 1:
-                    String login;
-                    String password;
-                    String tituloMensagem;
-                    String email;
-                    String mensagem;
-                    String mensagemEdit;
-                    System.out.println("Digite seu login:");
-                    sc.nextLine();
-                    login = sc.nextLine();
-                    System.out.println("Digite sua senha:");
-                    password = sc.nextLine();
-                    Properties props = new Properties();
-                    props.put("mail.smtp.host", "smtp.gmail.com");
-                    props.put("mail.smtp.port", "587");
-                    props.put("mail.smtp.auth", "true");
-                    props.put("mail.smtp.starttls.enable", "true");
+                    while(true){
+                        String login;
+                        String password;
+                        String tituloMensagem;
+                        String email;
+                        String mensagem;
+                        String mensagemEdit;
+                        System.out.println("Digite seu login:");
+                        sc.nextLine();
+                        login = sc.nextLine();
+                        System.out.println("Digite sua senha:");
+                        password = sc.nextLine();
+                        Properties props = new Properties();
+                        props.put("mail.smtp.host", "smtp.gmail.com");
+                        props.put("mail.smtp.port", "587");
+                        props.put("mail.smtp.auth", "true");
+                        props.put("mail.smtp.starttls.enable", "true");
 
-                    Session session = Session.getInstance(props, new Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(login, password);
-                        }
-                    });
-
-                    System.out.printf("Digite o Titulo da sua mensagem:");
-                    sc.nextLine();
-                    tituloMensagem = sc.nextLine();
-                    System.out.printf("Digite o email para enviar:");
-                    email = sc.nextLine();
-                    System.out.print("Digite a mensagem:");
-                    mensagem = sc.nextLine();
-                    System.out.printf("Deseja editar sua mensagem?:");
-                    mensagemEdit = sc.nextLine();
-                    Message message = new MimeMessage(session);
-                    message.setFrom(new InternetAddress(login));
-                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-                    message.setSubject(tituloMensagem);
-                    message.setText(mensagem);
-                    Transport.send(message);
-                    System.out.println("E-mail enviado com sucesso!");
-                    if(mensagemEdit.equals("Sim") || mensagemEdit.equals("sim") || mensagemEdit.equals("SIM")){
-                        System.out.printf("Reescreva por favor:");
-                        mensagem = sc.nextLine();
-                        System.out.println("Mensagem enviada para:" + email + ", com o titulo:" + tituloMensagem + "com a seguinte mensagem:" + mensagem);
-                    }else {
-                        if (eValido(email)) {
-                            String Continuar;
-                            System.out.println("Mensagem enviada para:" + email + ", com o titulo:" + tituloMensagem + "com a seguinte mensagem:" + mensagem);
-                            System.out.printf("Deseja enviar outro email?: (Sim ou nao)");
-                            Continuar = sc.nextLine();
-                            if (Continuar.equals("Sim")) {
-                                System.out.println("Digite o Titulo da sua mensagem:");
-                                tituloMensagem = sc.nextLine();
-                                System.out.println("Digite o email para enviar:");
-                                email = sc.nextLine();
-                                System.out.print("Digite a mensagem:");
-                                mensagem = sc.nextLine();
-                                sc.close();
-                            } else {
-                                System.out.println("Finalizando o programa");
-                                break;
+                        Session session = Session.getInstance(props, new Authenticator() {
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(login, password);
                             }
-                        } else {
-                            System.out.println("Email invalido! Finalizando...");
-                        }
-                    }
+                        });
+
+                        System.out.printf("Digite o Titulo da sua mensagem:");
+                        sc.nextLine();
+                        tituloMensagem = sc.nextLine();
+                        System.out.printf("Digite o email para enviar:");
+                        email = sc.nextLine();
+                        System.out.print("Digite a mensagem:");
+                        mensagem = sc.nextLine();
+                        System.out.printf("Deseja editar sua mensagem?:");
+                        mensagemEdit = sc.nextLine();
+                        Message message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress(login));
+                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+                        message.setSubject(tituloMensagem);
+                        message.setText(mensagem);
+                        Transport.send(message);
+                        System.out.println("E-mail enviado com sucesso!");
+                        if(mensagemEdit.equals("Sim") || mensagemEdit.equals("sim") || mensagemEdit.equals("SIM")){
+                            System.out.printf("Reescreva por favor:");
+                            mensagem = sc.nextLine();
+                            System.out.println("Mensagem enviada para:" + email + ", com o titulo:" + tituloMensagem + "com a seguinte mensagem:" + mensagem);
+                        }else {
+                            if (eValido(email)) {
+                                String Continuar;
+                                System.out.println("Mensagem enviada para:" + email + ", com o titulo:" + tituloMensagem + "com a seguinte mensagem:" + mensagem);
+                                System.out.printf("Deseja enviar outro email?: (Sim ou nao)");
+                                Continuar = sc.nextLine();
+                                if (Continuar.equals("Sim")) {
+                                    System.out.println("Digite o Titulo da sua mensagem:");
+                                    tituloMensagem = sc.nextLine();
+                                    System.out.println("Digite o email para enviar:");
+                                    email = sc.nextLine();
+                                    System.out.print("Digite a mensagem:");
+                                    mensagem = sc.nextLine();
+                                    sc.close();
+                                } else {
+                                    System.out.println("Finalizando o programa");
+                                    break;
+                                }
+                            } else {
+                                System.out.println("Email invalido! Finalizando...");
+                            }
+                        }}
+                    break;
+                case 2:
+                    System.out.println("Finalizando o programa");
+                    break;
+                default:
+                    System.out.println("Finalizando o programa");
                     break;
             }
         } catch(RuntimeException e) {
